3acdff4667142224b30b172be7aeaa1551dac879
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2023-07-19 12:38:09 -0300

    Aula 04: Operadores Matemáticos

---
+++ aula04.js
@@ -0,0 +1,28 @@
+// Aula 04: Operadores Matemáticos
+let num1 = 0;
+let num2 = 0;
+let res = 0;
+
+num1 = 10;
+num2 = 5;
+
+res = (num1 + num2) * 2;
+
+console.log(res);
+
+num1 = 10;
+num2 = 4;
+
+res = num1 % num2;
+
+console.log(res);
+
+res++;
+
+console.log(res);
+
+res--;
+console.log(res);
+
+res += 5;
+console.log(res);
