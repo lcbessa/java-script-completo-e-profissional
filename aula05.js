2938687b06b4cfefbffaf6ae5aab3bb994a3c1f8
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2023-08-23 18:10:38 -0300

    Aula 05: Operadores Relacionais

---
+++ aula05.js
@@ -0,0 +1,11 @@
+// Aula 05: Operadores Relacionais
+let num1 = 10;
+let num2 = 5;
+let num3 = 10;
+
+console.log(num1 > num2);
+console.log(num1 >= num2);
+console.log(num1 < num2);
+console.log(!(num1 == num3));
+
+// > >= < <= != == 
