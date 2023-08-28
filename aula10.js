1f7283fc1deb06a30bb7076e8638a288197481df
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2023-08-28 11:00:24 -0300

    Aula 10: Aprendendo sobre operador ternário

---
+++ aula10.js
@@ -0,0 +1,17 @@
+// Aula 10: Aprendendo sobre operador ternário
+let num = 10;
+
+// if (res == 0) {
+//   console.log("Número par");
+// } else {
+//   console.log("Número ímpar");
+// }
+
+res = !(num % 2) ? "Par" : "Ímpar";
+console.log(res);
+
+
+// 0 - False
+// 1 - True
+
+// Teste lógico ? se verdadeiro : se falso
