9d48ff0322496f386cddf03e0fd9fb23ddc4a2fe
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2023-08-28 11:35:34 -0300

    Aula 12: Operador Spread, aprendendo sobre o espalhador

---
+++ aula12.js
@@ -0,0 +1,30 @@
+// Aula 12: Operador Spread, aprendendo sobre o espalhador
+const objs1 = document.getElementsByTagName("div");
+const objs2 = [...document.getElementsByTagName("div")];
+let n1 = [10, 20, 30];
+let n2 = [11, 22, 33, 44, 55];
+let n3 = [...n1];
+console.log("n1: " + n1);
+console.log("n2: " + n2);
+console.log("n3: " + n3);
+
+const jogador1 = { nome: "Bruno", energia: 100, vidas: 3, magia: 150 };
+const jogador2 = { nome: "Bruce", energia: 100, vidas: 5, velocidade: 80 };
+const jogador3 = { ...jogador1, ...jogador2 };
+
+console.log(jogador3);
+
+const soma = (v1, v2, v3) => {
+  return v1 + v2 + v3;
+};
+console.log(soma(1, 5, 4));
+
+let valores = [10, 20, 30];
+
+console.log(soma(...valores));
+
+objs2.forEach((element) => {
+  console.log(element);
+});
+console.log(objs1);
+console.log(objs2);
