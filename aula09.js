b79ed1593a3a36387ab511f8b675ec8c94fa066d
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2023-08-28 10:00:03 -0300

    Aula 09: Diferença entre Pré Incremento e Pós Incremento

---
+++ aula09.js
@@ -0,0 +1,10 @@
+// Aula 09: Diferença entre Pré Incremento e Pós Incremento
+let n1 = 10;
+let n2 = 10;
+let n3 = 10;
+let n4 = 10;
+
+console.log(n1++);
+console.log(++n2);
+console.log(n3--);
+console.log(--n4);
