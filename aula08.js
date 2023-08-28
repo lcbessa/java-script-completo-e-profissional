0e9ea909c4cfc499004e9a17c37e36677812abb9
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2023-08-28 09:53:15 -0300

    Aula 08: Operadores Bitwise

---
+++ aula08.js
@@ -0,0 +1,14 @@
+// Aula 08: Operadores Bitwise
+let n1 = 10;
+let n2 = 11;
+let n3 = 10; // Define n3 aqui
+let n4 = 30;
+let AND = n1 | n2;
+let OR = n1 & n2;
+let BE = n3 << 1;
+let BD = n4 >> 1;
+
+console.log(AND);
+console.log(OR);
+console.log(BE);
+console.log(BD);
