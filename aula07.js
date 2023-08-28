e2a4328bf0025f44989275e19ebbbfa796c17f79
Luiz Carlos Bessa de Lima <luizcarlosbessa9@gmail.com>
2023-08-28 09:22:30 -0300

    Aula 07: Operadores Lógicos

---
+++ aula07.js
@@ -0,0 +1,12 @@
+// Aula 07: Operadores Lógicos
+let n1, n2, n3, n4;
+n1 = 10;
+n2 = 5;
+n3 = 15;
+n4 = 2;
+
+if (n1 > n2 || n3 > n4) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
